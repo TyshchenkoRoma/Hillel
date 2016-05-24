605bf0ada0ec665c1f449ff70e8b4b61c5133500
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-05-24 13:25:16 +0300

    deep clonning

--- hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Main.java
+++ hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Main.java
@@ -1,14 +1,41 @@
 package com.hillel.tyshchenko;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         Point point = new Point(1, 1);
         Rectangle rectangle = new Rectangle(2, 3, 4, 5);
+        int array[] = {1, 2, 3, 4, 5, 6, 7};
+        Point arrayPoints[] = {new Point(0, 0), new Point(1, 1), new Point(2, 2), new Point(3, 3)};
+
+        Point arrayPointsClone[] = arrayPoints.clone();
         Point point1 = point.clone();
         Rectangle rectangle1 = rectangle.clone();
-        System.out.println(rectangle1);
+        int array1[] = array.clone();
+
+        System.out.println(" point is: " + point + "  rectangle is: " + rectangle);
+        System.out.println("point1 is: " + point1 + " rectangle1 is: " + rectangle1);
+        System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(array1));
+        System.out.println(Arrays.toString(arrayPoints) + "\n" + Arrays.toString(arrayPointsClone));
+
+        point1.setX(0);
+        point1.setY(0);
         rectangle1.setLeftTop(point1);
-        System.out.println(rectangle1);
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] *= 2;
+        }
+        for (int i = 0; i < arrayPointsClone.length; i++) {
+            arrayPointsClone[i]= arrayPointsClone[i].addAllParameters(arrayPointsClone[i]) ;
+        }
+
+
+        System.out.println("\n point is: " + point + "  rectangle is: " + rectangle);
+        System.out.println("point1 is: " + point1 + " rectangle1 is: " + rectangle1);
+        System.out.println("point1 is: " + point1 + " rectangle1 is: " + rectangle1);
+        System.out.println(Arrays.toString(array) + "\n" + Arrays.toString(array1));
+        System.out.println(Arrays.toString(arrayPoints) + "\n" + Arrays.toString(arrayPointsClone));
+
     }
 }
--- hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Point.java
+++ hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Point.java
@@ -1,5 +1,9 @@
 package com.hillel.tyshchenko;
 
+import com.sun.xml.internal.bind.v2.TODO;
+
+import java.util.ArrayList;
+
 /**
  * Created by User on 07.04.2016.
  */
@@ -9,18 +13,24 @@ public class Point implements Cloneable{
     private double y;
 
     public Point() {
+
     }
 
     public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
-
+public Point addAllParameters (Point point){
+    setX(point.getX()+10);
+    setY(point.getY()+10);
+        return point;
+    }
     public double getX() {
         return x;
     }
 
     public void setX(double x) {
+
         this.x = x;
     }
 
@@ -29,13 +39,17 @@ public class Point implements Cloneable{
     }
 
     public void setY(double y) {
+
         this.y = y;
     }
 
     @Override
     public Point clone()  {
         try {
-            return (Point)super.clone();
+            Point temp = (Point)super.clone();
+         //   TODO  ????????????????????????????????????
+            return temp;
+
         }catch( CloneNotSupportedException ex){
             return null;
         }
