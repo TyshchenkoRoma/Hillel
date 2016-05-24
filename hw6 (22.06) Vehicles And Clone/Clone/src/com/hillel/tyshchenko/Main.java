f67115c0c8aee163e52930b04ace3ebca847df12
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-05-24 12:06:56 +0300

    add first step

--- hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Main.java
+++ hw6 (22.06) Vehicles And Clone/Clone/src/com/hillel/tyshchenko/Main.java
@@ -3,6 +3,12 @@ package com.hillel.tyshchenko;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Point point = new Point(1, 1);
+        Rectangle rectangle = new Rectangle(2, 3, 4, 5);
+        Point point1 = point.clone();
+        Rectangle rectangle1 = rectangle.clone();
+        System.out.println(rectangle1);
+        rectangle1.setLeftTop(point1);
+        System.out.println(rectangle1);
     }
 }
