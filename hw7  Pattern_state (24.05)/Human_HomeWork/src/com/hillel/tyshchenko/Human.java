b8813547f8164118badb72feaf0decb64120cdb9
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-05-31 13:17:29 +0300

    add opportunity  doing two thinks in one plase

--- hw7  Pattern_state (24.05)/Human_HomeWork/src/com/hillel/tyshchenko/Human.java
+++ hw7  Pattern_state (24.05)/Human_HomeWork/src/com/hillel/tyshchenko/Human.java
@@ -1,16 +1,21 @@
 package com.hillel.tyshchenko;
 
+import java.util.ArrayList;
+
 /**
  * Created by roman on 27.05.16.
  */
 public class Human {
-    State plase;
+    ArrayList<State> states = new ArrayList<>();
 
     public void setState(State pl) {
-        this.plase = pl;
+        this.states.add(pl);
     }
 
     public void takeSomething() {
-        plase.takeSomething();
+        for (State st : states) {
+            st.takeSomething();
+        }
+        states.clear();
     }
 }
--- hw7  Pattern_state (24.05)/Human_HomeWork/src/com/hillel/tyshchenko/Main.java
+++ hw7  Pattern_state (24.05)/Human_HomeWork/src/com/hillel/tyshchenko/Main.java
@@ -4,12 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
         Human human = new Human();
+
         State riverCoast = new RiversCoast();
         human.setState(riverCoast);
         human.takeSomething();
+        System.out.println();
+
         State wood = new Wood();
         human.setState(wood);
+        human.setState(riverCoast);
         human.takeSomething();
+        System.out.println();
+
         State enimal = new Enimal();
         human.setState(enimal);
         human.takeSomething();
