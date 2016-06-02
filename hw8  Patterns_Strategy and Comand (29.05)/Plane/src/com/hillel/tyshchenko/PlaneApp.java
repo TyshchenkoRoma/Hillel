cd5345aaaf62f4c5694ce7e110546758bda88d3f
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-06-02 13:20:52 +0300

    add bombarder

--- hw8  Patterns_Strategy and Comand (29.05)/Plane/src/com/hillel/tyshchenko/PlaneApp.java
+++ hw8  Patterns_Strategy and Comand (29.05)/Plane/src/com/hillel/tyshchenko/PlaneApp.java
@@ -9,8 +9,10 @@ public class PlaneApp {
         PlaneStates psGround = new Ground();
         PlaneStates psShoot = new Shoot();
         PlaneStates psAmunition = new Amunition();
-        //PlaneStates psMasinGun = new MachineGun();
+        PlaneStates psMasinGun = new MachineGun();
         PlaneStates psRocket = new Rocket();
+        PlaneStates psPermitionToShoot = new PermissionToShoot();
+        PlaneStates psIsAlient = new YourAlient();
 
         Plane plane = new Plane(psFly);
 
@@ -40,6 +42,22 @@ public class PlaneApp {
 
         plane.setPlaneStates(psShoot);
         plane.performAction(psShoot);
+        System.out.println();
+        System.out.println("BOMBARDER -----------------------------\n");
+
+        Bomber bomber = new Bomber(psFly);
+        bomber.setPlaneStates(psFly);
+        bomber.performAction(psFly);
+        bomber.setPlaneStates(psShoot);
+        bomber.performAction(psShoot);
+        bomber.setPlaneStates(psPermitionToShoot);
+        bomber.performAction(psPermitionToShoot);
+        bomber.setPlaneStates(psShoot);
+        bomber.performAction(psShoot);
+        bomber.setPlaneStates(psIsAlient);
+        bomber.performAction(psIsAlient);
+        bomber.setPlaneStates(psShoot);
+        bomber.performAction(psShoot);
     }
 }
 
@@ -139,5 +157,46 @@ class Gun implements Weaponry, PlaneStates {
 class Rocket implements Weaponry, PlaneStates {
     public void action() {
         System.out.println("Rocket is loaded");
+    }
+}
+class PermissionToShoot implements PlaneStates {
+    public void action() {
+        System.out.println("I have permission to shoot");
+    }
+}
+class YourAlient implements PlaneStates {
+    public void action() {
+        System.out.println("I know who is alient");
+    }
+}
+
+
+class Bomber extends Plane{
+    private boolean permissionToShoot;
+    private boolean yourAlient;
+
+    public Bomber(PlaneStates planeStates) {
+        super(planeStates);
+    }
+    public void setPlaneStates(PlaneStates planeStates) {
+        this.planeStates = planeStates;
+    }
+    void performAction(PlaneStates ps) {
+        if (ps instanceof Shoot && !permissionToShoot ) {
+            System.out.println(" You can't shoot now! Bombarder have not permission to shoot");
+            return;
+        }
+        if (ps instanceof Shoot && !yourAlient ) {
+            System.out.println(" You can't shoot now! Bombarder hasn't who is alient");
+            return;
+        }
+        if (ps instanceof PermissionToShoot) {
+            permissionToShoot = true;
+        }
+        if (ps instanceof YourAlient) {
+            yourAlient = true;
+        }
+
+        ps.action();
     }
 }
