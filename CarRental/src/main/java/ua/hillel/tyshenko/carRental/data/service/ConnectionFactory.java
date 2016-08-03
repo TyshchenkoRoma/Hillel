e868d945c9e74cbdaf33e931c45fb438651904fd
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-08-03 13:01:22 +0300

    created sql making file

---
+++ CarRental/src/main/java/ua/hillel/tyshenko/carRental/data/service/ConnectionFactory.java
@@ -0,0 +1,75 @@
+package ua.hillel.tyshenko.carRental.data.service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+/**
+ * Created by roman on 03.08.16.
+ */
+public class ConnectionFactory {
+    // database properties initialization
+    public static final String PATH_TO_PROPERTIES = System.getProperty("user.dir") + ".git/src/main/java/ua/kiev/allexb/carrental/data/service/db.properties";
+    private static final Properties properties;
+    static {
+        InputStream inputStream;
+        Properties prop = new Properties();
+        try {
+            inputStream = new FileInputStream(PATH_TO_PROPERTIES);
+            prop.load(inputStream);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        properties = prop;
+    }
+
+    //static reference to itself
+    private static ConnectionFactory instance = new ConnectionFactory();
+
+    //created connection
+    private static Connection dbConnection = null;
+
+    //private constructor
+    private ConnectionFactory() {
+        try {
+            String dbDriver = properties.isEmpty()? null: properties.getProperty("dbDriver");
+            Class.forName(dbDriver);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Connection createConnection() {
+        Connection connection = null;
+        try {
+            if (properties.isEmpty()) {
+                System.out.println("ERROR: Database properties could not load..");
+            } else {
+                String connectionUrl = properties.getProperty("connectionUrl");
+                String userName = properties.getProperty("userName");
+                String password = properties.getProperty("password");
+                connection = DriverManager.getConnection(connectionUrl, userName, password);
+            }
+        } catch (SQLException e) {
+            System.out.println("ERROR: Unable to Connect to Database.");
+        }
+        return connection;
+    }
+
+    public static Connection getConnection() {
+        try {
+            if (dbConnection == null || dbConnection.isClosed())
+                dbConnection = instance.createConnection();
+        } catch (SQLException e) {
+            System.out.println("ERROR: Unable to Connect to Database.");
+        }
+        return dbConnection;
+    }
+}
