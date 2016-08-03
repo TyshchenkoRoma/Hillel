751d1ccd641e8167070442b9dce626f9837c696a
Tyshchenko Roma <tyshenko.roma@gmail.com>
2016-08-03 11:24:18 +0300

    add pom and Main

---
+++ CarRental/src/main/java/ua/hillel/tyshenko/carRental/model/Main.java
@@ -0,0 +1,11 @@
+package ua.hillel.tyshenko.carRental.model;
+
+/**
+ * Created by roman on 03.08.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("TEST for working Maven project" );
+    }
+}
