3688daf95f34885797a4bf74c8072a7053840fc8
TyshchenkoRoma <tyshenko.roma@gmail.com>
2016-09-12 11:31:44 +0300

    com from IDEA added Validator

---
+++ CarRental/src/main/java/ua/hillel/tyshenko/carRental/controller/validator/Validator.java
@@ -0,0 +1,25 @@
+package ua.hillel.tyshenko.carRental.controller.validator;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by roman on 12.09.16.
+ */
+public abstract class Validator <T> {
+    protected HttpServletRequest request;
+    protected String message = "";
+    public Validator(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void addMessage(String massage) {
+        this.message += massage;
+    }
+
+    public abstract boolean validate();
+    public abstract T getValue();
+}
