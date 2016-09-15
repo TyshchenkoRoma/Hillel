96bf1983a366109be24096cf197dff6b39d6c3b6
TyshchenkoRoma <tyshenko.roma@gmail.com>
2016-09-15 13:56:06 +0300

    light refactoring

--- CarRental/src/main/java/ua/hillel/tyshenko/carRental/controller/AdministratorListServlet.java
+++ CarRental/src/main/java/ua/hillel/tyshenko/carRental/controller/AdministratorListServlet.java
@@ -4,7 +4,6 @@ import org.apache.log4j.Logger;
 import ua.hillel.tyshenko.carRental.data.dao.AdministratorDAO;
 import ua.hillel.tyshenko.carRental.data.dao.AdministratorDAOImpl;
 import ua.hillel.tyshenko.carRental.data.domain.AdministratorDomain;
-import ua.hillel.tyshenko.carRental.data.service.ConnectionFactory;
 import ua.hillel.tyshenko.carRental.model.Administrator;
 import ua.hillel.tyshenko.carRental.utils.ApplicationLogger;
 import ua.hillel.tyshenko.carRental.utils.StoreAndCookieUtil;
@@ -31,31 +30,25 @@ public class AdministratorListServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        //    Administrator loginedAdministrator = StoreAndCookieUtil.getLoginedAdministrator(request.getSession());
-      //      if (loginedAdministrator != null) {
-                logger.info("Extracting of administrator list from database.");
-                List<Administrator> administrators = null;
-                Connection connection = StoreAndCookieUtil.getStoredConnection(request);
-                try {
-                    AdministratorDAO administratorDAO = new AdministratorDAOImpl(connection);
-                    administrators = administratorDAO.getAll().stream().map(AdministratorDomain::getAdministrator).collect(Collectors.toList());
-                    logger.info("Administrator list extracted.");
-                } catch (SQLException ex) {
-                    logger.warn("Data base exception.", ex);
-                    request.setAttribute("javax.servlet.error.exception", ex);
-                    request.setAttribute("javax.servlet.error.status_code", 500);
-                    response.setStatus(500);
-                }
-                request.setAttribute("administratorList", administrators);
-                RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/adminListView.jsp");
-                dispatcher.forward(request, response);
-//            } else {
-//                logger.info("Request without authentication. Access denied.");
-//                request.setAttribute("errorString", "Access denied! Login previously.");
-//                RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
-//                dispatcher.forward(request, response);
-//            }
+
+        logger.info("Extracting of administrator list from database.");
+        List<Administrator> administrators = null;
+        Connection connection = StoreAndCookieUtil.getStoredConnection(request);
+        try {
+            AdministratorDAO administratorDAO = new AdministratorDAOImpl(connection);
+            administrators = administratorDAO.getAll().stream().map(AdministratorDomain::getAdministrator).collect(Collectors.toList());
+            logger.info("Administrator list extracted.");
+        } catch (SQLException ex) {
+            logger.warn("Data base exception.", ex);
+            request.setAttribute("javax.servlet.error.exception", ex);
+            request.setAttribute("javax.servlet.error.status_code", 500);
+            response.setStatus(500);
         }
+        request.setAttribute("administratorList", administrators);
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/adminListView.jsp");
+        dispatcher.forward(request, response);
+
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
--- CarRental/src/main/java/ua/hillel/tyshenko/carRental/controller/ClientListServlet.java
+++ CarRental/src/main/java/ua/hillel/tyshenko/carRental/controller/ClientListServlet.java
@@ -4,8 +4,6 @@ import org.apache.log4j.Logger;
 import ua.hillel.tyshenko.carRental.data.dao.ClientDAO;
 import ua.hillel.tyshenko.carRental.data.dao.ClientDAOImpl;
 import ua.hillel.tyshenko.carRental.data.domain.ClientDomain;
-import ua.hillel.tyshenko.carRental.data.service.ConnectionFactory;
-import ua.hillel.tyshenko.carRental.model.Administrator;
 import ua.hillel.tyshenko.carRental.model.Client;
 import ua.hillel.tyshenko.carRental.utils.ApplicationLogger;
 import ua.hillel.tyshenko.carRental.utils.StoreAndCookieUtil;
@@ -33,32 +31,28 @@ public class ClientListServlet extends HttpServlet {
     public ClientListServlet() {
         super();
     }
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-      //  Administrator loginedAdministrator = StoreAndCookieUtil.getLoginedAdministrator(request.getSession());
-      //  if (loginedAdministrator != null) {
-            logger.info("Extracting of client list list from database.");
-            List<Client> clientList = null;
-            Connection connection = StoreAndCookieUtil.getStoredConnection(request);
-            try {
-                ClientDAO clientDAO = new ClientDAOImpl(connection);
-                clientList = clientDAO.getAll().stream().map(ClientDomain::getClient).collect(Collectors.toList());
-                logger.info("Client list list extracted.");
-            } catch (SQLException ex) {
-                logger.warn("Data base exception.", ex);
-                request.setAttribute("javax.servlet.error.exception", ex);
-                request.setAttribute("javax.servlet.error.status_code", 500);
-                response.setStatus(500);
-            }
-            request.setAttribute("clientList", clientList);
-            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/clientListView.jsp");
-            dispatcher.forward(request, response);
-       /* } else {
-            logger.info("Request without authentication. Access denied.");
-            request.setAttribute("errorString", "Access denied! Login previously.");
-            RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/loginView.jsp");
-            dispatcher.forward(request, response);
-        }*/
+
+        logger.info("Extracting of client list list from database.");
+        List<Client> clientList = null;
+        Connection connection = StoreAndCookieUtil.getStoredConnection(request);
+        try {
+            ClientDAO clientDAO = new ClientDAOImpl(connection);
+            clientList = clientDAO.getAll().stream().map(ClientDomain::getClient).collect(Collectors.toList());
+            logger.info("Client list list extracted.");
+        } catch (SQLException ex) {
+            logger.warn("Data base exception.", ex);
+            request.setAttribute("javax.servlet.error.exception", ex);
+            request.setAttribute("javax.servlet.error.status_code", 500);
+            response.setStatus(500);
+        }
+        request.setAttribute("clientList", clientList);
+        RequestDispatcher dispatcher = this.getServletContext().getRequestDispatcher("/WEB-INF/views/clientListView.jsp");
+        dispatcher.forward(request, response);
+
     }
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request, response);
     }
